8c62fbd05a27259c24db59019269189f92ecf433
getsaad <saadibnbayazid@gmail.com>
2024-11-22 23:05:21 +0600

    todays last update

--- Variables, Data Types & Objects/script.js
+++ Variables, Data Types & Objects/script.js
@@ -1,6 +1,35 @@
-console.log("Hello World!")
-
 // variables
 
+// var variable
+console.log("Var Variable");
 var a = 5;
+var b = "Saad Ibn Bayazid"
+var a = 10;
+console.log(a, b);
+console.log(typeof a, typeof b, "\n");
 
+// let variable 
+console.log("let variable");
+let d =50;
+let e = "55.5";
+d = 90;
+console.log(d, e);
+console.log(typeof d, typeof e, "\n");
+
+// const variable
+console.log("const variable");
+const f = 100;
+// f = 55; it's not possible
+console.log(f);
+console.log(typeof f, "\n");
+
+// Objects
+
+console.log("object starting from here : ");
+let o = {
+    name : "Saad Ibn Bayazid",
+    designation : "Programmer",
+    "is handsome" : true,
+}
+
+console.log(o);
