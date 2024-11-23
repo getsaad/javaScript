e2b5b0f02a3f382ae28de25ac905387354b9e9f0
getsaad <saadibnbayazid@gmail.com>
2024-11-23 12:21:40 +0600

    Addes Class 2

---
+++ Class 2 conditionals/script.js
@@ -0,0 +1,34 @@
+// Conditionals 
+console.log("Conditionals is starting from here");
+console.log("if else if & else ladder!");
+
+let a = 5;
+if(a> 10) {
+    console.log("a is greater than 10");
+}
+
+if(a < 10) {
+    console.log("a is less than 10");
+}
+
+else if(a == 10){
+    console.log("a is equal to 10");
+}
+
+else{
+    console.log("I don't know");
+}
+
+console.log("Operators are starting from here!");
+a = 5;
+let b = 6;
+let c = 7;
+
+console.log(a+b);
+console.log(a+c);
+console.log(a-c);
+console.log(a*c);
+console.log(a*c);
+console.log(c/a);
+console.log("Exponential operator \"**\"", a ** 2);
+console.log("Check code with harry's note for more!");
