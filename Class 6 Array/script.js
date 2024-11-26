1b9f161f3b2568ccb16f2834e2fd6f6aae972b8f
getsaad <saadibnbayazid@gmail.com>
2024-11-27 00:00:42 +0600

    Array

---
+++ Class 6 Array/script.js
@@ -0,0 +1,16 @@
+let arr = [1,2,3,4,5,6,7]
+console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr.toString());
+// console.log(arr.join(" and "));
+
+console.log(arr.pop());
+console.log(arr.toString());
+console.log(arr.push("end"));
+console.log(arr.toString());
+
+console.log(arr.shift());
+console.log(arr.toString());
+console.log(arr.unshift("Start"));
+console.log(arr.toString());
